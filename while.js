1d66298f77343b8d0b3904a8aa82ee159ee4d08f
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-02 02:06:12 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,10 @@
+var roasteGiven = 0;
+
+while(roasteGiven < 9){
+    console.log("Roast den please!!")
+    console.log(roasteGiven);
+    roasteGiven++;
+
+//roasteGiven = roastGiven +1;
+//roastGiven += 1;
+}
