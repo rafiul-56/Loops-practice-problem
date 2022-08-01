1c0c3aa65f166a62a7f431f38f28540b35d1a594
Rafiul Islam <mdrafiulislam06@gmail.com>
2022-08-02 03:16:13 +0600

    for

---
+++ for.js
@@ -0,0 +1,30 @@
+// var roasteGiven = 0;
+// while(roasteGiven < 7) {
+//     console.log('Roast Den please!!');
+//     roasteGiven ++;
+// }
+
+// for loop
+// for(var roasteGiven = 0; roasteGiven < 7 ; roasteGiven ++){
+//     console.log('Roast den joldi khabo')
+// }
+
+// simple version of for loop
+// console.log('Simple for loop');
+/* for(var i = 0; i < 7 ; i ++){
+    console.log(i);
+} */
+
+// for(var i = 1 ; i <= 10 ; i ++){
+//     console.log(i);
+// }
+
+// i = i+2;
+//i+= 2;
+for(var i = 0; i <=20 ; i+=2){
+    console.log(i);
+}
+
+for(var i = 1; i <=20 ; i+=2){
+    console.log(i);
+}
